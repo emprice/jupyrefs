34df4e48fc562ba835fefff3070f01436b7b10ba
Ellen M. Price <emprice@uchicago.edu>
2022-04-17 20:59:12 -0500

    Note to self: Always run lint first!

--- README.md
+++ README.md
@@ -75,9 +75,9 @@ folder is located. Then you can remove the symlink named `jupyrefs` within that 
 
 See [RELEASE](RELEASE.md)
 
-# Credits
+## Credits
 
- + Icon by [FreeVector.com](https://www.freevector.com/chemistry-symbols-vector-27726),
-   with slight modification for compatibility with JupyterLab's light/dark theme settings
+- Icon by [FreeVector.com](https://www.freevector.com/chemistry-symbols-vector-27726),
+  with slight modification for compatibility with JupyterLab's light/dark theme settings
 
 <!-- vim: set ft=markdown: -->
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {
-    JupyterFrontEnd,
-    JupyterFrontEndPlugin
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 import { MainAreaWidget } from '@jupyterlab/apputils';
 import { ILauncher } from '@jupyterlab/launcher';
@@ -14,78 +14,82 @@ import { Widget } from '@lumino/widgets';
 import jupyrefsIconStr from './assets/iconmod.svg';
 
 const jupyrefsIcon = new LabIcon({
-    name: 'jupyrefs:icon',
-    svgstr: jupyrefsIconStr
+  name: 'jupyrefs:icon',
+  svgstr: jupyrefsIconStr
 });
 
 /**
  * Create the main content widget
  */
 function createMainWidget(app: JupyterFrontEnd): Widget {
-    const content = new Widget();
-    const widget = new MainAreaWidget({ content });
-    widget.id = 'jupyrefs:main';
-    widget.title.label = 'Reference Manager';
-    widget.title.closable = true;
+  const content = new Widget();
+  const widget = new MainAreaWidget({ content });
+  widget.id = 'jupyrefs:main';
+  widget.title.label = 'Reference Manager';
+  widget.title.closable = true;
 
-    if (!widget.isAttached) {
-        app.shell.add(widget, 'main');
-    }
+  if (!widget.isAttached) {
+    app.shell.add(widget, 'main');
+  }
 
-    return widget;
+  return widget;
 }
 
 /**
  * Create the file browser widget
  */
-function createFileBrowserWidget(app: JupyterFrontEnd,
-    factory: IFileBrowserFactory): FileBrowser {
-        const widget = factory.createFileBrowser('jupyrefs:browse');
-        widget.title.icon = jupyrefsIcon;
+function createFileBrowserWidget(
+  app: JupyterFrontEnd,
+  factory: IFileBrowserFactory
+): FileBrowser {
+  const widget = factory.createFileBrowser('jupyrefs:browse');
+  widget.title.icon = jupyrefsIcon;
 
-        if (!widget.isAttached) {
-            app.shell.add(widget, 'left');
-        }
+  if (!widget.isAttached) {
+    app.shell.add(widget, 'left');
+  }
 
-        return widget;
+  return widget;
 }
 
 /**
  * Initialization data for the jupyrefs extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-    id: 'jupyrefs:plugin',
-    autoStart: true,
-    requires: [ILauncher, IFileBrowserFactory],
-    activate: async (app: JupyterFrontEnd, launcher: ILauncher,
-        factory: IFileBrowserFactory) => {
+  id: 'jupyrefs:plugin',
+  autoStart: true,
+  requires: [ILauncher, IFileBrowserFactory],
+  activate: async (
+    app: JupyterFrontEnd,
+    launcher: ILauncher,
+    factory: IFileBrowserFactory
+  ) => {
+    // Add the command to the app
+    const command = 'jupyrefs:open';
+    app.commands.addCommand(command, {
+      label: 'Start Reference Manager',
+      icon: jupyrefsIcon,
+      execute: () => {
+        const browser = createFileBrowserWidget(app, factory);
+        const main = createMainWidget(app);
 
-        // Add the command to the app
-        const command: string = 'jupyrefs:open';
-        app.commands.addCommand(command, {
-            label: 'Start Reference Manager',
-            icon: jupyrefsIcon,
-            execute: () => {
-                const browser = createFileBrowserWidget(app, factory);
-                const main = createMainWidget(app);
-
-                // Define the cleanup for the main widget
-                main.disposed.connect((sender, args) => {
-                    browser.dispose();
-                });
-
-                // All done -- activate!
-                app.shell.activateById(main.id);
-            }
+        // Define the cleanup for the main widget
+        main.disposed.connect((sender, args) => {
+          browser.dispose();
         });
 
-        // Add the command to the launcher
-        launcher.add({
-            command: command,
-            category: 'Reference Manager',
-            rank: 0
-        });
-    }
+        // All done -- activate!
+        app.shell.activateById(main.id);
+      }
+    });
+
+    // Add the command to the launcher
+    launcher.add({
+      command: command,
+      category: 'Reference Manager',
+      rank: 0
+    });
+  }
 };
 
 export default plugin;
