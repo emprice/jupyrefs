ca6bf4e62a1981d5db63cbe13ddccb7d9e36e5fb
Ellen M. Price <emprice@uchicago.edu>
2022-05-06 09:45:22 -0500

    Nested directories appear nested in listing

--- client/src/dirlisting.ts
+++ client/src/dirlisting.ts
@@ -9,15 +9,23 @@ import {
   TranslationBundle,
   nullTranslator
 } from '@jupyterlab/translation';
+import { Contents } from '@jupyterlab/services';
+
+import dirIconStr from '!./assets/icon_folder.svg';
+import pdfIconStr from '!./assets/icon_document_pdf.svg';
 
 const containerClass = 'dirlisting';
+const rootClass = 'root';
 const branchClass = 'branch';
 const itemClass = 'item';
+const itemIconClass = 'itemicon';
 const itemTextClass = 'itemtext';
 
 const selectableClass = 'mod-selectable';
 const selectedClass = 'mod-selected';
 const expandedClass = 'mod-expanded';
+
+const iconMap = new Map<string, string>([['application/pdf', pdfIconStr]]);
 
 interface IOptions {
   model: IBrowserModel;
@@ -40,6 +48,8 @@ export class JupyrefsDirListing extends Widget {
 
   public async refresh(): Promise<void> {
     const content = await this.buildTree();
+    content.classList.remove(makeClass(containerClass, branchClass));
+    content.classList.add(makeClass(containerClass, rootClass));
     this.node.replaceChildren(content);
   }
 
@@ -49,6 +59,109 @@ export class JupyrefsDirListing extends Widget {
 
   public get fileOpened(): Signal<Widget, string> {
     return this._fileOpened;
+  }
+
+  protected directoryHandler(list: HTMLElement): (m: Contents.IModel) => void {
+    return (item: Contents.IModel) => {
+      const elem = document.createElement('li');
+      const row = document.createElement('div');
+
+      const icon = document.createElement('span');
+      icon.innerHTML = dirIconStr || '';
+      icon.classList.add(makeClass(containerClass, itemIconClass));
+      row.appendChild(icon);
+
+      const name = document.createElement('span');
+      name.innerHTML = item.name;
+      name.classList.add(makeClass(containerClass, itemTextClass));
+      row.appendChild(name);
+
+      row.dataset.path = item.path;
+      row.dataset.mimetype = '';
+      row.dataset.isdir = 'true';
+      row.dataset.expanded = 'false';
+
+      row.classList.add(makeClass(containerClass, itemClass));
+      row.classList.add(makeClass(selectableClass));
+      elem.appendChild(row);
+
+      row.addEventListener('click', event => {
+        const target = event.currentTarget as HTMLElement;
+        const cls = makeClass(selectedClass);
+
+        const selected = this.node.querySelectorAll(`.${cls}`);
+        for (const e of selected) {
+          e.classList.remove(cls);
+        }
+
+        target.classList.add(cls);
+      });
+
+      row.addEventListener('dblclick', async event => {
+        if (event.currentTarget) {
+          const target = event.currentTarget as HTMLElement;
+          const cls = makeClass(expandedClass);
+
+          if (target.dataset.expanded === 'false') {
+            target.classList.add(cls);
+            const sublist = await this.buildTree(target.dataset.path);
+            elem.appendChild(sublist);
+            target.dataset.expanded = 'true';
+          } else if (target.dataset.expanded === 'true') {
+            target.classList.remove(cls);
+            elem.replaceChildren(target);
+            target.dataset.expanded = 'false';
+          }
+        }
+      });
+
+      list.appendChild(elem);
+    };
+  }
+
+  protected fileHandler(list: HTMLElement): (m: Contents.IModel) => void {
+    return (item: Contents.IModel) => {
+      const elem = document.createElement('li');
+      const row = document.createElement('div');
+
+      const icon = document.createElement('span');
+      icon.innerHTML = iconMap.get(item.mimetype) || '';
+      icon.classList.add(makeClass(containerClass, itemIconClass));
+      row.appendChild(icon);
+
+      const name = document.createElement('span');
+      name.innerHTML = item.name;
+      name.classList.add(makeClass(containerClass, itemTextClass));
+      row.appendChild(name);
+
+      row.dataset.path = item.path;
+      row.dataset.mimetype = item.mimetype;
+      row.dataset.isdir = 'false';
+      row.classList.add(makeClass(containerClass, itemClass));
+      row.classList.add(makeClass(selectableClass));
+      elem.appendChild(row);
+
+      row.addEventListener('click', event => {
+        const target = event.currentTarget as HTMLElement;
+        const cls = makeClass(selectedClass);
+
+        const selected = this.node.querySelectorAll(`.${cls}`);
+        for (const e of selected) {
+          e.classList.remove(cls);
+        }
+
+        target.classList.add(cls);
+      });
+
+      row.addEventListener('dblclick', async event => {
+        const target = event.currentTarget as HTMLElement;
+        if (target.dataset.path) {
+          this._fileOpened.emit(target.dataset.path);
+        }
+      });
+
+      list.appendChild(elem);
+    };
   }
 
   protected async buildTree(rootPath = ''): Promise<Element> {
@@ -67,66 +180,12 @@ export class JupyrefsDirListing extends Widget {
 
       list.appendChild(elem);
     } else {
-      response.forEach(item => {
-        const elem = document.createElement('li');
-        const row = document.createElement('div');
-
-        const name = document.createElement('span');
-        name.innerHTML = item.name;
-        name.classList.add(makeClass(containerClass, itemTextClass));
-
-        row.appendChild(name);
-
-        row.dataset.path = item.path;
-        row.dataset.mimetype = item.mimetype;
-        row.dataset.isdir = (item.type === 'directory').toString();
-        row.classList.add(makeClass(containerClass, itemClass));
-        row.classList.add(makeClass(selectableClass));
-
-        elem.appendChild(row);
-
-        row.addEventListener('click', event => {
-          const target = event.currentTarget as HTMLElement;
-          const cls = makeClass(selectedClass);
-
-          const selected = list.querySelectorAll(`.${cls}`);
-          for (const e of selected) {
-            e.classList.remove(cls);
-          }
-
-          target.classList.add(cls);
-        });
-
-        if (item.type === 'file') {
-          row.addEventListener('dblclick', async event => {
-            const target = event.currentTarget as HTMLElement;
-            if (target.dataset.path) {
-              this._fileOpened.emit(target.dataset.path);
-            }
-          });
-        } else if (item.type === 'directory') {
-          row.dataset.expanded = 'false';
-          row.addEventListener('dblclick', async event => {
-            if (event.currentTarget) {
-              const target = event.currentTarget as HTMLElement;
-              const cls = makeClass(expandedClass);
-
-              if (target.dataset.expanded === 'false') {
-                target.classList.add(cls);
-                const sublist = await this.buildTree(target.dataset.path);
-                elem.appendChild(sublist);
-                target.dataset.expanded = 'true';
-              } else if (target.dataset.expanded === 'true') {
-                target.classList.remove(cls);
-                elem.replaceChildren(target);
-                target.dataset.expanded = 'false';
-              }
-            }
-          });
-        }
-
-        list.appendChild(elem);
-      });
+      response
+        .filter((m: Contents.IModel) => m.type === 'directory')
+        .forEach(this.directoryHandler(list));
+      response
+        .filter((m: Contents.IModel) => m.type === 'file')
+        .forEach(this.fileHandler(list));
     }
 
     return list;
